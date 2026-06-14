655e4a1216364fadf628fad3bb3c180bb02acc4b
Jefri Sembiring <jefri.123140206@student.itera.ac.id>
2026-06-14 23:06:06 +0700

    Update UI Test

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.kotlinSerialization)
     alias(libs.plugins.sqldelight)
+    id("org.jetbrains.kotlinx.kover") version "0.8.0"
 }
 
 // Load local.properties for API keys
